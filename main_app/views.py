8ca6ad95e1fc0d3d884ff06187b083da2f5233a7
Mohamed Meshaima <mohamedoaderu@gmail.com>
2025-09-07 16:51:13 +0300

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
+
+# Create your views here.
+def home(request):
+    # Send a simple HTML response
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
